befd0f4e76156a660e0a9909a86a52f88496cab1
Jadan morrow <jadanmorrow@outlook.com>
2020-08-07 07:34:47 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace returnStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cubedNumber = cube(5);
+            Console.WriteLine(cubedNumber);
+            //Console.WriteLine(cube(5));
+            Console.ReadLine();
+
+        }
+
+        static int cube(int num)
+        {
+            int result = num * num * num;
+            return result;
+        }
+    }
+}
